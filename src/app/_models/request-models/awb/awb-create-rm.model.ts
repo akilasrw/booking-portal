97083fd58876774f82936ca91329a9179f3cc265
rajitha_live <rajithatcc@gmail.com>
2022-06-10 00:37:23 +0530

    SCS-126 done

--- src/app/_models/request-models/awb/awb-create-rm.model.ts
+++ src/app/_models/request-models/awb/awb-create-rm.model.ts
@@ -1,30 +1,31 @@
 import { AWBProductRM } from "./awb-product-rm.model";
 
-export interface AWBCreateRM{
-  userId:string
-    shipperName:string;
-    shipperAccountNumber:string;
-    shipperAddress:string;
-    consigneeName:string;
-    consigneeAccountNumber:string;
-    consigneeAddress:string;
-    agentName:string;
-    agentCity:string;
-    agentAITACode:string;
-    agentAccountNumber:string;
-    agentAccountInformation:string;
-    requestedRouting:string;
-    routingAndDestinationTo:string;
-    routingAndDestinationBy:string;
-    requestedFlightDate:Date;
-    destinationAirportId:string;
-    destinationAirportCode:string;
-    shippingReferenceNumber:string;
-    currency:string;
-    declaredValueForCarriage:number;
-    declaredValueForCustomer:number;
-    amountOfInsurance:number;   
-    packageProducts:AWBProductRM[];
-    isPackageUpdate:boolean;
+export class AWBCreateRM{
+  userId?:string
+    shipperName?:string;
+    shipperAccountNumber?:string;
+    shipperAddress?:string;
+    consigneeName?:string;
+    consigneeAccountNumber?:string;
+    consigneeAddress?:string;
+    agentName?:string;
+    agentCity?:string;
+    agentAITACode?:string;
+    agentAccountNumber?:string;
+    agentAccountInformation?:string;
+    requestedRouting?:string;
+    routingAndDestinationTo?:string;
+    routingAndDestinationBy?:string;
+    requestedFlightDate?:Date;
+    destinationAirportId?:string;
+    destinationAirportCode?:string;
+    shippingReferenceNumber?:string;
+    currency?:string;
+    declaredValueForCarriage?:number;
+    declaredValueForCustomer?:number;
+    amountOfInsurance?:number;   
+    packageProducts?:AWBProductRM[];
+    isPackageUpdate?:boolean;
+    isEditAWB?:boolean;
   }
   
