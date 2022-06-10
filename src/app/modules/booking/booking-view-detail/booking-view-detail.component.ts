57f7422c384f0bb990133f9a083aa05e4f3cf004
rajitha_live <rajithatcc@gmail.com>
2022-06-10 16:40:34 +0530

    SCS-134 done partly

--- src/app/modules/booking/booking-view-detail/booking-view-detail.component.ts
+++ src/app/modules/booking/booking-view-detail/booking-view-detail.component.ts
@@ -4,6 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BookingService } from 'src/app/_services/booking.service';
 import { CoreExtensions } from 'src/app/core/extensions/core-extensions.model';
 import { BookingStatus, PackageItemStatus } from 'src/app/core/enums/common-enums';
+import { AWBCreateRM } from 'src/app/_models/request-models/awb/awb-create-rm.model';
 
 @Component({
   selector: 'app-booking-view-detail',
@@ -14,6 +15,10 @@ export class BookingViewDetailComponent implements OnInit {
 
   @Input() cargoBookingId?: string;
   cargoBookingDetail?: CargoBookingDetail
+  modalVisible = false;
+  modalVisibleAnimate = false;
+  awbModel?:AWBCreateRM;
+
 
   constructor(private bookingSerice: BookingService) { }
 
@@ -45,6 +50,29 @@ export class BookingViewDetailComponent implements OnInit {
 
   GetFormattedAWBNumber(value: number): string {
     return value == 0 ? '-' : CoreExtensions.PadLeadingZeros(value, 8);
+  }
+
+  addAWBFromAddPackage(){
+    this.awbModel = new AWBCreateRM();
+    this.awbModel.isPackageUpdate = false;
+
+    this.modalVisible = true;
+    setTimeout(() => (this.modalVisibleAnimate = true));
+  }
+
+  closeAWBForm() {
+    this.modalVisibleAnimate = false;
+    setTimeout(() => (this.modalVisible = false), 300);
+  }
+
+  submitAWBDetail(awb: AWBCreateRM) {
+    // awb.userId = this.currentUser?.id != null ? this.currentUser?.id : "";
+    // this.awbDetail = awb;
+    // if (this.awbModel != null && this.awbModel?.isPackageUpdate && this.selectedPackage != null && this.selectedPackageIndex != undefined) {
+    //   this.selectedPackage.aWBDetail = awb;
+    //   this.selectedPackage.packageItemStatus = PackageItemStatus.AddedAWB;
+    //   this.cargoBookingRequest.packageItems?.splice(this.selectedPackageIndex!, 1, this.selectedPackage)
+    // }
   }
 
   get bookingStatus(): typeof BookingStatus {
