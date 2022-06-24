119c9f7c2afaed72a0c4fa27e20ae4673cab0f72
rajitha_live <rajithatcc@gmail.com>
2022-06-24 17:19:24 +0530

    Add stg env

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseEndpoint: 'https://aeroclub-skytechcargo-app-dev-002-staging.azurewebsites.net/api/v1/',
+    encriptionKey:"123456$#@$^@1ERF"
+};
