8ffd64a47714b11a6819e11f1493caca21feeb80
rajitha_live <rajithatcc@gmail.com>
2022-09-01 15:16:01 +0530

    SCS-173

--- src/app/modules/booking-lookup/booking-lookup-search/booking-lookup-search.component.ts
+++ src/app/modules/booking-lookup/booking-lookup-search/booking-lookup-search.component.ts
@@ -2,7 +2,7 @@ import { AWBStatus } from './../../../core/enums/common-enums';
 import { AccountService } from 'src/app/account/account.service';
 import { CargoBookingLookup } from './../../../_models/view-models/cargo-booking-lookup/cargo-booking-lookup.model';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CargoBookingLookupQuery } from 'src/app/_models/queries/cargo-booking-lookup/cargo-booking-lookup-query.model';
 import { BookingLookupService } from 'src/app/_services/booking-lookup.service';
 import { CoreExtensions } from 'src/app/core/extensions/core-extensions.model';
@@ -10,9 +10,6 @@ import { ToastrService } from 'ngx-toastr';
 import { BookingStatus, PackageItemStatus } from 'src/app/core/enums/common-enums';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { User } from 'src/app/_models/user.model';
-import { BookingService } from 'src/app/_services/booking.service';
-import { CargoBookingDetail } from 'src/app/_models/view-models/cargo-booking/cargo-booking-detail/cargo-booking-detail.model';
-import { PackageItem } from 'src/app/_models/view-models/package-item.model';
 import { AWBDetail } from 'src/app/_models/view-models/awb/awb-detail.model';
 import { BookingLookupPrintComponent } from '../booking-lookup-print/booking-lookup-print.component';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -25,7 +22,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class BookingLookupSearchComponent implements OnInit {
 
-  public searchForm!:FormGroup;
+  public searchForm!: FormGroup;
   cargoBookingLookup?: CargoBookingLookup
   packageItemStatus = PackageItemStatus;
   subscription?: Subscription;
@@ -33,10 +30,10 @@ export class BookingLookupSearchComponent implements OnInit {
   awsPrintLookup?: AWBDetail;
   isPrinting: boolean = false;
 
-  
-  @ViewChild(BookingLookupPrintComponent ) child !:any ;
 
-  constructor(private fb:FormBuilder,
+  @ViewChild(BookingLookupPrintComponent) child !: any;
+
+  constructor(private fb: FormBuilder,
     private bookingLookupService: BookingLookupService,
     private accountService: AccountService,
     private toastr: ToastrService,
@@ -47,52 +44,69 @@ export class BookingLookupSearchComponent implements OnInit {
     this.initializeForm();
   }
 
-  initializeForm(){
+  initializeForm() {
     this.searchForm = this.fb.group({
       referenceNumber: new FormControl(null)
     });
   }
 
-
-
-  getBookingDetail(){
-   
-    if(this.searchForm.value.referenceNumber != null){
+  getBookingDetail() {
+    if (this.searchForm.value.referenceNumber != null) {
       var query = new CargoBookingLookupQuery;
       query.referenceNumber = this.searchForm.value.referenceNumber;
-      query.isIncludeFlightDetail=true;
-      query.isIncludePackageDetail=true;
-      query.isIncludeAWBDetail=true;
+      query.isIncludeFlightDetail = true;
+      query.isIncludePackageDetail = true;
+      query.isIncludeAWBDetail = true;
       query.userId = this.currentUser?.id
 
       this.bookingLookupService.getBookingLookupDetail(query).subscribe(
         {
           next: (res) => {
             this.cargoBookingLookup = res;
+
+            var packages = this.cargoBookingLookup.packageItems;
+            this.cargoBookingLookup.packageItems = [];
+
+            var filtered = packages.filter((value, index, self) =>
+              index === self.findIndex((t) => (
+                t.length === value.length && t.width === value.width && t.weight === value.weight
+              ))
+            )
+
+            for (let i = 0; i < filtered.length; i++) {
+              var count = 0;
+              for (let j = 0; j < packages.length; j++) {
+                if (filtered[i].length === packages[j].length && 
+                  filtered[i].width === packages[j].width && 
+                  filtered[i].height === packages[j].height) {
+                  count += 1;
+                }
+              }
+              filtered[i].pieces = count;
+            }
+            this.cargoBookingLookup.packageItems = filtered;
           },
           error: (error) => {
             this.cargoBookingLookup = undefined;
           }
         });
-
-    }else{
+    } else {
       this.toastr.error('Please enter booking number or package number.');
     }
-    
   }
 
-  getBookingStatus(status:number):string{
+  getBookingStatus(status: number): string {
     return CoreExtensions.GetBookingStatus(status)
   }
 
-  getAWBStatus(status:number):string{
+  getAWBStatus(status: number): string {
     return CoreExtensions.GetAWBStatus(status)
   }
 
   GetFormattedAWBNumber(cargoBookingLookup: CargoBookingLookup): string {
-    if(cargoBookingLookup != null && cargoBookingLookup.awbInformation != null && cargoBookingLookup.awbInformation.awbTrackingNumber != undefined){
-      return cargoBookingLookup.awbInformation.awbTrackingNumber == 0? '-':CoreExtensions.PadLeadingZeros(cargoBookingLookup.awbInformation.awbTrackingNumber,8);
-    }else{
+    if (cargoBookingLookup != null && cargoBookingLookup.awbInformation != null && cargoBookingLookup.awbInformation.awbTrackingNumber != undefined) {
+      return cargoBookingLookup.awbInformation.awbTrackingNumber == 0 ? '-' : CoreExtensions.PadLeadingZeros(cargoBookingLookup.awbInformation.awbTrackingNumber, 8);
+    } else {
       return '-';
     }
   }
@@ -103,24 +117,24 @@ export class BookingLookupSearchComponent implements OnInit {
     });
   }
 
-  generatePDF(cargoBookingLookup: CargoBookingLookup) { 
-    this.isPrinting =true;
-    if(cargoBookingLookup.awbInformation !== undefined) {      
+  generatePDF(cargoBookingLookup: CargoBookingLookup) {
+    this.isPrinting = true;
+    if (cargoBookingLookup.awbInformation !== undefined) {
       this.spinner.show();
-      console.log('generatePDF');    
+      console.log('generatePDF');
       this.child.printData(cargoBookingLookup.awbInformation);
       this.isPrinting = false;
       setTimeout(() => {
         this.spinner.hide();
       }, 2000);
-    } 
+    }
   }
 
   get awbStatus(): typeof AWBStatus {
     return AWBStatus;
   }
 
-  get bookingStatus():typeof BookingStatus {
+  get bookingStatus(): typeof BookingStatus {
     return BookingStatus;
   }
 }
