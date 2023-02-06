ae4c09e67242a1002d20dc2f6c8f55088b409a4a
rajitha_live <rajithatcc@gmail.com>
2023-02-06 16:00:20 +0530

    Cargo booking detil changes.

--- src/app/_models/view-models/cargo-booking/cargo-booking-detail/cargo-booking-detail.model.ts
+++ src/app/_models/view-models/cargo-booking/cargo-booking-detail/cargo-booking-detail.model.ts
@@ -1,7 +1,6 @@
 import { AWBStatus } from './../../../../core/enums/common-enums';
 import { BookingStatus } from "src/app/core/enums/common-enums";
 import { AWBDetail } from "../../awb/awb-detail.model";
-import { FlightScheduleSector } from "../../flight-schedule-sectors/flight-schedule-sector.model";
 import { PackageItem } from "../../package-item.model";
 
 export interface CargoBookingDetail{
